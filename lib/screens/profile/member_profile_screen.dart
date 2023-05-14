ee34967e9cc358e64acc361e797353128797123e
Wasim <wasimxaman13@gmail.com>
2023-05-14 11:25:52 +0500

    project is up to date

--- lib/screens/profile/member_profile_screen.dart
+++ lib/screens/profile/member_profile_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, non_constant_identifier_names
+// ignore_for_file: library_private_types_in_public_api
 
 import 'package:event_management/constants/app_colors.dart';
 import 'package:event_management/controllers/profile/profile_controller.dart';
